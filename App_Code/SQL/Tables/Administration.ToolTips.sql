3c70524326d446c0345d1863b341e8107d3caccc
Rob Boyett <robin.boyett@hotmail.com>
2017-07-21 11:41:36 +0100

    New table to enable tooltips

---
+++ App_Code/SQL/Tables/Administration.ToolTips.sql
@@ -0,0 +1,21 @@
+USE [GoTournamental]
+GO
+
+/****** Object: Table [dbo].[Sponsors] Script Date: 28/03/2015 07:27:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Administration].[ToolTips] (
+    [ID]                    INT            IDENTITY (1, 1) NOT NULL,
+    [WebPage]				VARCHAR (200)  NOT NULL,
+    [ControlID]				VARCHAR (200)  NOT NULL,
+    [ToolTipText]           VARCHAR(MAX)   NOT NULL,
+    [UserID]				VARCHAR (200)  NOT NULL,
+	[DateAdded]				DATETIME NOT NULL
+    CONSTRAINT [PK_ToolTips] PRIMARY KEY ([ID])
+);
+
+
